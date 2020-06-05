6f5f475ebabb009e19ddc6f71c8cb0a13c0c7138
A.K <38154154+Ashutosh-Kukreti@users.noreply.github.com>
2020-06-05 11:22:46 +0530

    Script is rewritten with better comments
    Comments are improved and some commands are changed.

--- Package_cleaner.sh
+++ Package_cleaner.sh
@@ -1,5 +1,6 @@
 #! /bin/bash 
-#A simple bash script for cleaning packages in Manjaro Linux or Other Arch based linux distros [including ARCH Linux :) ]
+#A simple bash script for cleaning packages in Arch linux and  Arch based distros like Manjaro linux.
+
 #Colors used in script
 RED='\033[0;31m'
 NC='\033[0m'
@@ -7,34 +8,38 @@ WHITE="\033[1;37m"
 
 # echo is used to display text
 #-e - enable interpretation of backslash escapes
-echo -e "${WHITE}Listing Disk Usage${NC}\n"
-# df is used to report file system disk space usage.
-df -h
+echo -e "${WHITE}Listing Size of Package directory${NC}\n"
+# du is used to summarize disk usage of the /var/cache/pacman/pkg folder.
+du -h /var/cache/pacman/pkg | grep "/var/cache/pacman/pkg"
 
 echo -e "\n${WHITE}Cleaning unused versions of all packages ${NC}\n"
-#pamac is a tool available on Manjaro Linux,it is easier to use than pacman
-pamac clean -v
-#verbose is enabled in pamac
+#pamac is a package manager utility developed by Manjaro Linux team,it is easier to use than pacman
+pamac clean 
+#To remove older version of packages from cache
 
-echo -e "\n${WHITE}List and Remove Orphan Packages${NC}\n"
+echo -e "\n${WHITE}Listing Orphan Packages${NC}\n"
+#This requires internet
 pamac list -o
+
+echo -e "\n${WHITE}Removing Orphan Packages${NC}\n"
 pamac remove -o
 
-echo -e "\n${WHITE}Clean AUR Build files${NC}\n"
+echo -e "\n${WHITE}Cleaning AUR build files${NC}\n"
 pamac clean -b -v
+#Build directory is the one specified in pamac.conf
 
-echo -e "\n${RED}Need Root privileges${NC}\n"
-#pacman - package manager utility of ARCH Linux
-echo -e "\n${WHITE}To Delete all the cached packages in /var/cache/pacman/pkg/${NC}"
-echo -e "\n${RED}Remove repo will remove unused db files from /var/lib/pacman/sync${NC}\n"
-sudo pacman -Sc
+echo -e "\n${RED}Need Root privileges${NC}"
+echo -e "${WHITE}Pacman will delete all the cached packages in /var/cache/pacman/pkg/${NC}"
+echo -e "${WHITE}Remove repo will remove unused db files from /var/lib/pacman/sync${NC}"
+echo -e "\n${RED}Package Downgrade will not be possible after this${NC}"
 
-echo -e "\n${WHITE}Delete all cached packages including the version installed${NC}"
-echo -e "\n${RED}Package Downgrade will not be possible after this${NC}\n"
+#pacman - default package manager utility of ARCH Linux
+#This command will clear the entire cache directory.
 sudo pacman -Scc
 
-echo -e "\n${WHITE}Listing Disk Usage${NC}\n"
-df -h
+echo -e "${WHITE}Listing Size of Package directory${NC}\n"
+du -h /var/cache/pacman/pkg | grep "/var/cache/pacman/pkg"
 
 echo -e "\n${WHITE}Cleaning Complete${NC}"
-#Yes, I am a n00b.
+
+#Yes I'm a n00b.
